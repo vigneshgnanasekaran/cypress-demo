fd067daddb1225d18a6314a0ed3068095413cfde
vigneshgnanasekaran <vigneshg@francium.tech>
2024-01-17 17:19:32 +0530

    made changes in test case assertion in url

--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,10 +1,10 @@
 describe("example to-do app", () => {
   beforeEach(() => {
-    cy.visit("https://www.undivided.io");
+    cy.visit("https://www.undivided.io/");
   });
 
   it("checking the opening of the page", () => {
-    cy.url().should("eq", "https://www.undivided.io");
+    cy.url().should("eq", "https://www.undivided.io/");
     cy.log("hi your welcome ");
   });
 });
