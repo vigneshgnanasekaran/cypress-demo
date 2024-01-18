2a68784c58ce237a0580821274b288ef50dbf403
vigneshgnanasekaran <vigneshg@francium.tech>
2024-01-18 14:30:56 +0530

    add few test cases

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
-const { defineConfig } = require("cypress");
-const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-
-module.exports = defineConfig({
+module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      allureWriter(on, config);
-      return config;
     },
   },
-});
+};
--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -1,10 +1,18 @@
 describe("example to-do app", () => {
   beforeEach(() => {
-    cy.visit("https://www.undivided.io/");
+    cy.visit("https://qa.teamspecialx.com/");
   });
 
   it("checking the opening of the page", () => {
-    cy.url().should("eq", "https://www.undivided.io/");
-    cy.log("hi your welcome ");
+    cy.url().should("eq", "https://qa.teamspecialx.com/");
+    cy.get('a').each((link) => {
+      
+      const href = link.attr('href');
+
+      
+      cy.request(href).then((response) => {
+        expect(response.status).to.eq(200); // Assuming a successful response code
+      });
+    });
   });
 });
