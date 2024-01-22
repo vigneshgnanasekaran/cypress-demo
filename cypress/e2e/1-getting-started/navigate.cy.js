ee554735ab26ef7dd3d3c0fe4ee8cde196fc889f
vigneshgnanasekaran <vigneshg@francium.tech>
2024-01-22 11:05:35 +0530

    add one more test navigate to check the parallel runing in circle ci

--- cypress/e2e/1-getting-started/navigate.cy.js
+++ cypress/e2e/1-getting-started/navigate.cy.js
@@ -7,8 +7,9 @@ describe("example navigate app app", () => {
   beforeEach(() => {
     cy.visit("https://qa.teamspecialx.com/resources/decoders");
   });
-
+  context(`Description : ${articleDetails.description}`, () => {
   it("checking the opening of the page", () => {
     cy.url().should("eq", "https://qa.teamspecialx.com/resources/decoders");
   });
+});
 });
