a46b51f251df181bcd588932eca16b857f0b7bca
vigneshgnanasekaran <vigneshg@francium.tech>
2024-01-22 10:58:17 +0530

    add one more test navigate to check the parallel runing in circle ci

---
+++ cypress/e2e/1-getting-started/navigate.cy.js
@@ -0,0 +1,14 @@
+describe("example navigate app app", () => {
+  const articleDetails = {
+    description:
+      "Verifying article text,tag, default image, and content existence for a single article ID or the latest 10 articles",
+  };
+
+  beforeEach(() => {
+    cy.visit("https://qa.teamspecialx.com/resources/decoders");
+  });
+
+  it("checking the opening of the page", () => {
+    cy.url().should("eq", "https://qa.teamspecialx.com/resources/decoders");
+  });
+});
--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -5,18 +5,15 @@ describe("example to-do app", () => {
 
   it("checking the opening of the page", () => {
     cy.url().should("eq", "https://qa.teamspecialx.com/");
-    cy.get('a').each((link) => {
-      
-      const href = link.attr('href');
+    cy.get("a").each((link) => {
+      const href = link.attr("href");
 
-      if(href=="mailto:support@undivided.io"){
-         return
+      if (href == "mailto:support@undivided.io") {
+        return;
       }
       cy.request(href).then((response) => {
-        expect(response.status).to.eq(200);  
+        expect(response.status).to.eq(200);
       });
     });
   });
 });
-
- 
